752266a350a958d8058c2cea2b969eaf22c589b4
Zijian Zheng <zhengzijian56@gmail.com>
2019-10-03 23:48:09 +0100

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0, total = 0;
+		while (x < 10) {
+			System.out.print(x + " ");
+			x = x + 1;
+			
+		}
+		System.out.println(" ");
+	}
+}
